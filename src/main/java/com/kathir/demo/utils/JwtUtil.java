e34c19a2a372344b029e805db2de261b0aaedaa5
Kathir <your_email@example.com>
2025-12-06 14:54:47 +0530

    env files updated

--- src/main/java/com/kathir/demo/utils/JwtUtil.java
+++ src/main/java/com/kathir/demo/utils/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,13 @@ public class JwtUtil {
     @Value("${jwt.token.secret}")
     private String SECRET;
 
-    private final Key secretKey = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    private Key secretKey ;
+
+    @PostConstruct
+    public void keyInit(){
+        this.secretKey= Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    }
 
     public String generateToken(String email) {
 
